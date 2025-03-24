217b383f0e3da1e119f88b9f71e3ab8b9bc847ff
dijitald <drew.dials@outlook.com>
2025-03-24 12:12:12 -0400

    full service

--- function_app.py
+++ function_app.py
@@ -6,7 +6,7 @@ from function_app_context import context
 from sqlalchemy.orm import sessionmaker, scoped_session
 from service_models import engine
 from service_users import bpUsers
-# from service_households import bpHouseholds
+from service_households import bpHouseholds
 # from service_tasks import bpTasks
 # from service_activities import bpActivities
 # from service_memberships import bpMembers
@@ -18,7 +18,7 @@ app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 
 try:
     app.register_blueprint(bpUsers)
-    # app.register_blueprint(bpHouseholds)
+    app.register_blueprint(bpHouseholds)
     # app.register_blueprint(bpTasks)
     # app.register_blueprint(bpActivities)
     # app.register_blueprint(bpMembers)
